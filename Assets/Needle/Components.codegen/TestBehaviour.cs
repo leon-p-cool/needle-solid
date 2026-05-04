0278d48ba6d5d5a7a9c44cfc761eb8c895a431db
leon-p-cool <leonp@mailbox.org>
2026-05-04 14:46:01 +0200

    new script on Cube(1) to grow on click

---
+++ Assets/Needle/Components.codegen/TestBehaviour.cs
@@ -0,0 +1,18 @@
+// NEEDLE_CODEGEN_START
+// auto generated code - do not edit directly
+
+#pragma warning disable
+
+namespace Needle.Typescript.GeneratedComponents
+{
+	public partial class TestBehaviour : UnityEngine.MonoBehaviour
+	{
+		[UnityEngine.SerializeField]
+		private float @scaleFactor = 1.2f;
+		public void Start() {}
+		public void Update() {}
+		public void onPointerClick() {}
+	}
+}
+
+// NEEDLE_CODEGEN_END
