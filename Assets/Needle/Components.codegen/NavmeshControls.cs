723696465d240e3b8a79fdb827d4e1b9e6abc834
leon-p-cool <leonp@mailbox.org>
2026-05-11 17:25:32 +0200

    copy demo scripts into project, faulty rn

---
+++ Assets/Needle/Components.codegen/NavmeshControls.cs
@@ -0,0 +1,17 @@
+// NEEDLE_CODEGEN_START
+// auto generated code - do not edit directly
+
+#pragma warning disable
+
+namespace Needle.Typescript.GeneratedComponents
+{
+	public partial class NavmeshControls : UnityEngine.MonoBehaviour
+	{
+		public UnityEngine.GameObject @goalVisualizer;
+		public void Awake() {}
+		public void Update() {}
+		public void raycast() {}
+	}
+}
+
+// NEEDLE_CODEGEN_END
